0e622e43913e62d10d19af691449bc7a797b3469
robertcarr <roberttullycarr@gmail.com>
2021-07-08 14:52:51 +0200

    global styles changes

--- frontend/src/4_Components/1_Main/index.jsx
+++ frontend/src/4_Components/1_Main/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Main = styled.div`
   display: flex;
   height: auto;
+  min-height: 100vh;
   width: 100%;
   flex-direction: column;
   align-items: center;
