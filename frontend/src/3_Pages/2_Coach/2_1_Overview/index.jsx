06bad49aa082878dc2047e9b2a1147f02689f270
robertcarr <roberttullycarr@gmail.com>
2021-07-09 15:32:37 +0200

    route card built

--- frontend/src/3_Pages/2_Coach/2_1_Overview/index.jsx
+++ frontend/src/3_Pages/2_Coach/2_1_Overview/index.jsx
@@ -1,6 +1,7 @@
 import {Body, Main} from "../../../4_Components/1_Main";
 import MenuBar from "../../../4_Components/3_MenuBar";
 import HeaderBar from "../../../4_Components/2_HeaderBar";
+import SessionCard from "../../../4_Components/5_SessionCard";
 
 
 
@@ -10,7 +11,7 @@ const Overview = () => {
             <MenuBar />
             <Body>
                 <HeaderBar title={'OVERVIEW'}/>
-
+                <SessionCard/>
             </Body>
         </Main>
     )
--- frontend/src/4_Components/9_RouteCardSmall/index.jsx
+++ frontend/src/4_Components/9_RouteCardSmall/index.jsx
@@ -1,18 +1,16 @@
-import styled from "styled-components";
+import stelvio from "../../5_Assets/JPG/stelvio_06_hairpin.jpeg"
+import RouteDataSmall from "./RouteData";
+import {RouteCardMain, RouteImg, RouteImgMain} from "./styled";
 
-const RouteCardMain = styled.div`
-width: 90%;
-height: 22vw;
-background: ${props => props.theme.ELWhite};
-border: 1px solid #BDBDBD;
-border-radius: 5px;
-box-shadow: ${props => props.theme.BoxShadowWidget};
-`
+
 
 const RouteCardSmall = () => {
     return (
         <RouteCardMain>
-
+            <RouteImgMain>
+                <RouteImg src={stelvio}/>
+            </RouteImgMain>
+            <RouteDataSmall title={'Passo del Stelvio'} distance={21.5} avg_grade={7.4} elevation={1912} steep_grade={10.5}/>
         </RouteCardMain>
     )
 }
