f1fdd84245d9ce8bf7692e0333e6eec7bbe668bc
Nenad Haefeli <nenad.haefeli@gmail.com>
2021-07-09 14:34:14 +0200

    merged dev to nenad

--- frontend/src/3_Pages/2_Coach/2_5_Results/index.jsx
+++ frontend/src/3_Pages/2_Coach/2_5_Results/index.jsx
@@ -2,18 +2,12 @@ import {Body, Main} from "../../../4_Components/1_Main";
 import MenuBar from "../../../4_Components/3_MenuBar";
 import HeaderBar from "../../../4_Components/2_HeaderBar";
 
-// import SessionCardLarge from "../../../4_Components/6_SessionCardLarge";
-// import RouteOptions from "../../../4_Components/13_RouteOptions";
-
 const Results = () => {
     return (
         <Main>
             <MenuBar/>
             <Body>
                 <HeaderBar title={'RESULTS - THIJS DEKIERE'}/>
-                <h1>Coach - results page</h1>
-                {/*<SessionCardLarge/>*/}
-                {/*<RouteOptions/>*/}
             </Body>
         </Main>
     )
