73e7d56aa30f1f7efa5167021f0a12d8c92ed8cf
robertcarr <roberttullycarr@gmail.com>
2021-07-11 09:30:32 +0200

    component organization and compatability

--- frontend/src/3_Pages/2_Coach/2_6_Sessions/index.jsx
+++ frontend/src/3_Pages/2_Coach/2_6_Sessions/index.jsx
@@ -1,6 +1,8 @@
 import {Body, Main} from "../../../4_Components/1_Main";
 import MenuBar from "../../../4_Components/3_MenuBar";
 import HeaderBar from "../../../4_Components/2_HeaderBar";
+import SearchBar from "../../../4_Components/8_SearchBar";
+import SessionCard from "../../../4_Components/5_SessionCard";
 
 
 const Sessions = () => {
@@ -9,6 +11,14 @@ const Sessions = () => {
             <MenuBar/>
             <Body>
                 <HeaderBar title={'SESSIONS'}/>
+                <SessionCard/>
+                <SessionCard/>
+                <SessionCard/>
+                <SessionCard/>
+                <SessionCard/>
+                <SessionCard/>
+                <SessionCard/>
+                <SessionCard/>
             </Body>
         </Main>
     )
