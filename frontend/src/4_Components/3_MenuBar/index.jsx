215cc180aab66ef53aa040b24a81e90bec55b46c
robertcarr <roberttullycarr@gmail.com>
2021-07-15 12:44:32 +0200

    fixed olivia files

--- frontend/src/4_Components/3_MenuBar/index.jsx
+++ frontend/src/4_Components/3_MenuBar/index.jsx
@@ -1,23 +1,17 @@
 import {useHistory} from "react-router-dom";
 import EL_logo from '../../5_Assets/PNG/energylab_notext.png'
 import Avatar from "../7_Avatar";
-import profile_photo from "../../5_Assets/PNG/carr_profile.jpg"
 import MBLink from "./MBLinks";
 import { ReactComponent as LogoutIcon } from "../../5_Assets/SVG/43_navbarBtn.svg";
-
 import { ReactComponent as Overview } from '../../5_Assets/SVG/11_overview.svg';
 import { ReactComponent as Clients} from '../../5_Assets/SVG/12_clients.svg';
 import { ReactComponent as Routes } from '../../5_Assets/SVG/13_routes.svg';
 import { ReactComponent as Sessions } from '../../5_Assets/SVG/14_sessions.svg';
 
+import {BorderLine, LinksMainDiv, LogoImg, MenuBarMain, LogOutBtn, Wrapper,
+    WrapperLogo, LogoutContainer, LogoutText} from "./styled";
 
-import {BorderLine, LinksMainDiv, LogoImg, MenuBarMain, LogOutBtn, Wrapper, WrapperLogo, LogoutContainer, LogoutText} from "./styled";
 
-import overview from '../../5_Assets/SVG/11_overview.svg'
-import cyclist from '../../5_Assets/SVG/12_clients.svg'
-import routes from '../../5_Assets/SVG/13_routes.svg'
-import sessions from '../../5_Assets/SVG/14_sessions.svg'
-import {BorderLine, LinksMainDiv, LogoImg, MenuBarMain} from "./styled";
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchLoggedInUserData} from "../../2_Store/Fetches/logged_in_user_info";
