b55db83e56a31ee28b938e6dce272073c92ecec5
Robert Carr <roberttullycarr@gmail.com>
2021-07-15 09:36:54 +0000

    Revert "Merge branch 'olivia' into 'dev'"
    This reverts merge request !33

--- frontend/src/4_Components/3_MenuBar/MBLinks/index.jsx
+++ frontend/src/4_Components/3_MenuBar/MBLinks/index.jsx
@@ -1,15 +1,7 @@
 import styled from "styled-components";
-import {useHistory, NavLink} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 const MBLinks = styled.button`
-<<<<<<< HEAD
-width: 5vw;
-border: none;
-background: none;
-svg{
-    width: 50%;
-}
-=======
   width: 100%;
   height: 25%;
   justify-content: center;
@@ -25,50 +17,14 @@ svg{
 
 const Icon = styled.img`
   width: 50%;
->>>>>>> dev
 `
-const DivMenuLink = styled.div`
-    height: 15%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor:pointer;
-    &:hover{
-        //background: #0177B1;
-        background: #8bc34a;
-        p{
-            color: ${props => props.theme.ELWhite}!important;
-        } 
-    }
-    .main-nav-active{
-        svg{
-            fill: #8bc34a;
-        }
-        p{color: #8bc34a }
-       
-    }
-`;
-const NameNavBar = styled.p`
-    color: white;
-    font-weight: 700;
-    font-size: 2vw;
-`;
 
 const MBLink = (props) => {
     const history = useHistory();
     return (
-        
-            <DivMenuLink onClick={() => history.push(props.url)} >
-                <NavLink to={props.url} activeClassName="main-nav-active">
-                    <MBLinks>
-                        {props.icon}
-                    </MBLinks>
-                    <NameNavBar>{props.navbarname}</NameNavBar>
-                </NavLink>
-            </DivMenuLink>
-       
-        
-        
+        <MBLinks display={props.display} onClick={() => history.push(props.url)}>
+            <Icon src={props.icon} />
+        </MBLinks>
     )
 }
 
