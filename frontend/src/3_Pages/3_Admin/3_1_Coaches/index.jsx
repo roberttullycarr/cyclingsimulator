9e9c7b49953ffcb2363dcd5db56259b0f2d236e7
Karim Abdelaziz <karimabdelaziz@live.com>
2021-07-18 13:37:56 +0200

    compiled successfully

--- frontend/src/3_Pages/3_Admin/3_1_Coaches/index.jsx
+++ frontend/src/3_Pages/3_Admin/3_1_Coaches/index.jsx
@@ -4,21 +4,12 @@ import HeaderBar from "../../../4_Components/2_HeaderBar";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import { Container } from "../../../4_Components/8_SearchBar";
-import BaseButton from "../../../4_Components/4_ButtonsInputs/Button";
-import SearchBar from "../../../4_Components/8_SearchBar";
-import styled from "styled-components";
 import CoachCard from "../../../4_Components/28_CoachCard";
 import {fetchAllCoaches} from "../../../2_Store/Fetches/get_all_coaches";
 import {ReactComponent as SearchIcon} from "../../../5_Assets/SVG/40_search.svg";
 import NewClient from "../../../4_Components/12_NewClient";
 import Title from "../../../4_Components/14_Title";
 
-const ButtonWrapper = styled.div`
-  height: 3vw;
-  width: 85%;
-  display: flex;
-  justify-content: center;
-`
 
 const Coaches = () => {
 
@@ -34,6 +25,7 @@ const Coaches = () => {
         <Main>
             <MenuBar />
             <Body>
+
                 <HeaderBar title={'Admin'}/>
                 <Container>
                     <div className="helper">
@@ -42,17 +34,14 @@ const Coaches = () => {
                     <input placeholder="Search coaches by first name, last name, email or location"
                            onChange={(e) => setKeyword(e.target.value)}/>
                 </Container>
-                <ButtonWrapper>
-                    <BaseButton height={100} width={10} text={'New Coach'}/>
-                </ButtonWrapper>
-                {users ? users.map(user => {if (user['is_coach']) {return(<CoachCard user={user}/>)}}):null}
-                <SearchBar/>
+
                 <NewClient url={'coach/new/'} type={'NEW_COACH'} text={'New Coach'}/>
+
                 <SectionWrapper>
                     <Title text={'Coaches'}/>
-                    {users ? users.map(user => {if (user.id !== loggedInUser.id) {return(<CoachCard
-                        user={user} type={'coaches'} />)}}):null}
+                    {users ? users.map(user => <CoachCard user={user} type={'coaches'} />) :'Loading...'}
                 </SectionWrapper>
+
             </Body>
         </Main>
     )
